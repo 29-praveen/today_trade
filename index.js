bcdce48b92283aea5e472bdfa63c24c89fe86d83
29-praveen <74659785+29-praveen@users.noreply.github.com>
2023-02-13 23:14:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function sendMail() {
+    
+  var params = {
+    fullName: document.getElementById("fullName").value,
+    email_id: document.getElementById("email_id").value,
+    number: document.getElementById("number").value,
+    message: document.getElementById("message").value,
+  };
+ 
+  emailjs
+    .send("service_auf1vf6", "template_q2t2q7r", params)
+    .then(function (res) {
+      alert("send success!");
+    });
+}
+function Course() {
+  var fullName= document.getElementById("fullName").value
+  var email_id= document.getElementById("email_id").value
+  var number= document.getElementById("number").value
+  var message= document.getElementById("address").value
+  var course= document.getElementById("course").value
+  if(fullName=="" || email_id == "" || number=="" ||course == "" || message==""){
+    var w=document.getElementById("wrong");
+      w.innerHTML= "Please Fill The Details First"
+  }else{
+    var params = {
+      fullName: fullName,
+      email_id: email_id,
+      number: number,
+      address: message,
+      course: course,
+    };
+   
+    emailjs
+      .send("service_auf1vf6", "template_f75wnsg", params)
+      .then(function (res) {
+        alert("send success!");
+      });
+  }
+  
+}
